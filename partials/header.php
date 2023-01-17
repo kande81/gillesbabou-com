c79de217e17288a132b6f2b3b655c7f78d17d2ef
gilles <yannickbabou@gmail.com>
2023-01-17 05:05:35 -0800

    removed the address bar color

--- partials/header.php
+++ partials/header.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="theme-color" content="#f4acff" />
-    <meta name="browser-logo" content="#f4acff" />
 
     <link rel="stylesheet" href="https://use.typekit.net/zrk0gpj.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css" />
